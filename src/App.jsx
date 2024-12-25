e67a259e76a5f6833ba8d8dd876150eaa4bc3566
TYProject6 <laimaneha@gmail.com>
2024-12-25 22:18:03 +0530

    Initial commit for Vite React project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p className='h-96 bg-black'>this is paragraph</p>
+    </>
+  )
+}
+
+export default App
